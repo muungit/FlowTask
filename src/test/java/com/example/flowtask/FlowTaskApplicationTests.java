262759bc5fd7a2e808d4353aea8fde7de7a95f2b
munngit <munhs012@gmail.com>
2026-02-26 19:35:17 +0900

    init: bootstrap spring boot project

---
+++ src/test/java/com/example/flowtask/FlowTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
